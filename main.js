0d59b335aacabce3a1236f41ebc2809943f64d81
Jørund Jenssen Berg <jjberg@gmail.com>
2019-05-25 09:13:59 +0200

    Inserting unCipher button with corresponding reverse function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let btnCipher = document.querySelector('button');
+let btnCipher = document.getElementById('cipherIT');
+let btnUnCipher = document.getElementById('unCipher');
 let oldText = document.getElementById('oldOne');
 let newText = document.getElementById('newOne');
 let writtenText = document.querySelector('textarea');
@@ -18,8 +19,8 @@ btnCipher.onclick = function() {
                                  .replace(/u/gi, '?')
                                  .replace(/d/gi, '€')
                                  .replace(/p/gi, '+')
-                                 .replace(/m/gi, 'o')
-                                 .replace(/h/gi, 'p')
+                                 .replace(/m/gi, ';')
+                                 .replace(/h/gi, ':')
                                  .replace(/g/gi, 1)
                                  .replace(/b/gi, 2)
                                  .replace(/f/gi, 3)
@@ -31,6 +32,36 @@ btnCipher.onclick = function() {
                                  .replace(/z/gi, 9)
                                  .replace(/j/gi, 0)
                                  .replace(/q/gi, '@');
+}
+
+btnUnCipher.onclick = function() {
+  let inputText = writtenText.value;
+  newText.textContent = inputText.replace(/!/gi, 'e')
+                                 .replace(/""/gi, 'a')
+                                 .replace(/#/gi, 'r')
+                                 .replace(/\$/gi, 'i')
+                                 .replace(/%/gi, 'o')
+                                 .replace(/&/gi, 't')
+                                 .replace(/\//gi, 'n')
+                                 .replace(/\(/gi, 's')
+                                 .replace(/\)/gi, 'l')
+                                 .replace(/=/gi, 'c')
+                                 .replace(/\?/gi, 'u')
+                                 .replace(/€/gi, 'd')
+                                 .replace(/\+/gi, 'p')
+                                 .replace(/;/gi, 'm')
+                                 .replace(/:/gi, 'h')
+                                 .replace(/1/gi, 'g')
+                                 .replace(/2/gi, 'b')
+                                 .replace(/3/gi, 'f')
+                                 .replace(/4/gi, 'y')
+                                 .replace(/5/gi, 'w')
+                                 .replace(/6/gi, 'k')
+                                 .replace(/7/gi, 'v')
+                                 .replace(/8/gi, 'x')
+                                 .replace(/9/gi, 'z')
+                                 .replace(/0/gi, 'j')
+                                 .replace(/@/gi, 'q');
 }
 //0!1 p!&!# 0ø#?/€ 0!/((!/ 2!#1
 //jeg heter jørund jenssen berg
